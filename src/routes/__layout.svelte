61c2380a4ad3bf3db7e90bbb9d31e4b63c4a2441
Albin Groen <albin.groen@gmail.com>
2021-12-15 10:36:15 +0100

    refactor: SEO improvements

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,31 @@
 <script lang="ts">
 	import '../styles/tailwind-output.css';
+
+	const url = 'https://bostadskollen.albingroen.com';
+	const image =
+		'https://res.cloudinary.com/albin-groen/image/upload/q_auto,f_auto/v1639560937/bostadskollen-seo_vndez1.png';
+	const description =
+		'Med Bostadskollen kan du ta reda på vad en bostadsrätt skulle kosta dig varje månad.';
+	const title = 'Bostadskollen - Vad kostar bostaden?';
 </script>
+
+<svelte:head>
+	<title>Bostadskollen - Vad kostar bostaden?</title>
+	<meta name="title" content={title} />
+	<meta name="description" content={description} />
+
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={url} />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content={image} />
+
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content={url} />
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />
+	<meta property="twitter:image" content={image} />
+</svelte:head>
 
 <main class="antialiased">
 	<slot />
